ec167ab2641ff447c808e8d222b0269265cd5537
LucasLuis-Dev <lucasluisouza@gmail.com>
2024-03-01 14:56:06 -0300

    fix: correction to project images and profile picture that were inoperable

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,7 +1,7 @@
 export const dataFake = [
   {
     "id" : "1",
-    "photos": ["https://cdn.discordapp.com/attachments/965066624556232737/1190785129988239471/iphone-multiple-screens-mockup_2.png?ex=65a31012&is=65909b12&hm=6e7f5d24101d4ecaf53a986c46b0fab0a5538a0d9d90c7075d6a1a83ac8c48f3&","https://cdn.discordapp.com/attachments/965066624556232737/1191063068587864124/google-pixelbook-mockup_2.png?ex=65a412ec&is=65919dec&hm=3706dc2b043321b576c1db523c5cd7d3462940e542be30bb55657f767b5f479a&", "https://cdn.discordapp.com/attachments/965066624556232737/1191063070689210448/clean-minimalistic-notebook-mockup.png?ex=65a412ec&is=65919dec&hm=c2ea45e0a723aaa8caee2688089dd7b143b353f8845062f9652a960d86bee390&"],
+    "photos": ["../../../../../assets/images/dev-community-mobile.png","../../../../../assets/images/dev-community-notebook.png", "../../../../../assets/images/dev-community-netbook.png"],
     "title": "Dev Community",
     "description": "Desenvolvi o Dev Community, uma aplicação web que proporciona interação e colaboração entre desenvolvedores. Utilizei o framework Flask para criar a aplicação do zero, implementando recursos como autenticação de usuários e integração com banco de dados. ",
     "techs": ['python','flask', 'tailwindcss', 'sqlalchemy', 'git'],
@@ -21,7 +21,7 @@ export const dataFake = [
   */
   {
     "id": "2",
-    "photos": ["https://cdn.discordapp.com/attachments/965066624556232737/1190853407578263652/iphone-multiple-screens-mockup_3.png?ex=65a34fa9&is=6590daa9&hm=5aaeb4f6e5d55386f5881a9d0020dabc135f6ae683c4426381b5eaf759ae5648&","https://cdn.discordapp.com/attachments/965066624556232737/1191063068881461288/google-pixelbook-mockup_1.png?ex=65a412ec&is=65919dec&hm=8e3b30f877e7765f0dd6cc70ecf9a67b69a6cdc48148b0f0f7cf9c1b491344fc&", "https://cdn.discordapp.com/attachments/965066624556232737/1191063069749694646/clean-minimalistic-notebook-mockup_2.png?ex=65a412ec&is=65919dec&hm=0e07c184c7b1ed4eb30dac737585482f0d77c45bc011e0fbe0c4c395701097dd&"],
+    "photos": ["../../../../../assets/images/hotcoffee-mobile.png","../../../../../assets/images/hotcoffee-notebook.png", "../../../../../assets/images/hotcoffee-netbook.png"],
     "title": "HotCoffee",
     "description": "A aplicação Angular em desenvolvimento na Vercel é uma plataforma de busca de notícias de tecnologia, alimentada pela API do News API. Os usuários podem realizar pesquisas específicas e visualizar resultados de notícias de maneira intuitiva. ",
     "techs": ['git', 'angularjs','css3', 'typescript'],
@@ -30,7 +30,7 @@ export const dataFake = [
   },
   {
     "id": "3",
-    "photos": ["https://cdn.discordapp.com/attachments/965066624556232737/1190794774823845989/iphone-multiple-screens-mockup_4.png?ex=65a3190e&is=6590a40e&hm=b0c408f76aecec9a191cd486df6e5f978808350efbac34ba7192b9c5ecaf5265&","https://cdn.discordapp.com/attachments/965066624556232737/1191063068285870191/google-pixelbook-mockup_3.png?ex=65a412ec&is=65919dec&hm=185c43fa96e28af3c8d984645b8354ffc89fb07848833dcba27b75f3950bc18a&", "https://cdn.discordapp.com/attachments/965066624556232737/1191063067954532442/clean-minimalistic-notebook-mockup_3.png?ex=65a412ec&is=65919dec&hm=15c18810e67ed507ece2deba1edd051e4cd3a68783a99bf7f3dc0690de3d13a7&"],
+    "photos": ["../../../../../assets/images/algoritmo-kruskal-mobile.png","../../../../../assets/images/algoritmo-kruskal-notebook.png", "../../../../../assets/images/algoritmo-kruskal-netbook.png"],
     "title": "Algoritmo de Kruskal",
     "description": "Este projeto trata de um problema ilustrativo onde há preocupações de segurança da comunicação por e-mail em uma instituição, identificando conexões importantes e confiáveis entre os membros.",
     "techs": ['python', 'vscode', 'git'],
@@ -39,7 +39,7 @@ export const dataFake = [
   },
   {
     "id": "4",
-    "photos": ["https://cdn.discordapp.com/attachments/965066624556232737/1191824829079371887/iphone-multiple-screens-mockup_5.png?ex=65a6d85e&is=6594635e&hm=281fbb7940a768d55aca0dc024933f997a36c36f99db8ed34a8481feca4659b0&","https://cdn.discordapp.com/attachments/965066624556232737/1191063067321184276/google-pixelbook-mockup_4.png?ex=65a412ec&is=65919dec&hm=8dd416beee99a015327b9f024f0ffb45ff05af3896c2a9571c27173b088e8724&","https://cdn.discordapp.com/attachments/965066624556232737/1191063066671075418/clean-minimalistic-notebook-mockup_4.png?ex=65a412eb&is=65919deb&hm=25b50c785b5ff6ec431b6b38e361780fd704be52120272017192551d1c517bee&"],
+    "photos": ["../../../../../assets/images/space-attack-mobile.png","../../../../../assets/images/space-attack-notebook.png","../../../../../assets/images/space-attack-netbook.png"],
     "title": "Space Attack",
     "description": "Space Attack é um jogo interativo baseado em Star Wars, onde o jogador controla uma nave e enfrenta asteróides e naves inimigas.",
     "techs": ['python', 'git', 'vscode'],
@@ -48,7 +48,7 @@ export const dataFake = [
   },
   {
     "id": "5",
-    "photos": ["https://cdn.discordapp.com/attachments/965066624556232737/1190782363706347550/iphone-multiple-screens-mockup.png?ex=65a30d7f&is=6590987f&hm=f7a2abd28e655d8a5fc60a7c67ba25dffab3309acae4677123f3e1311d778f66&","https://cdn.discordapp.com/attachments/965066624556232737/1191063069175066705/google-pixelbook-mockup.png?ex=65a412ec&is=65919dec&hm=957161a5aae16863f992f19563600c7fa3ace0c4890921fc848f3686352018c7&", "https://cdn.discordapp.com/attachments/965066624556232737/1191063070202667179/clean-minimalistic-notebook-mockup_1.png?ex=65a412ec&is=65919dec&hm=b161dea8bb301d1556b203221d07631cbf3aa576eca083076148c3d0110850f8&"],
+    "photos": ["../../../../../assets/images/doctor-care-mobile.png","../../../../../assets/images/doctor-care-notebook.png", "../../../../../assets/images/doctor-care-netbook.png"],
     "title": "Doctor Care",
     "description": "O projeto consiste em um código que é capaz de navegar na web e coletar o preço e o link de compra dos produtos determinados na base de dados podendo ter um número indeterminado de produtos.",
     "techs": ['javascript', 'git','html5', 'css'],
