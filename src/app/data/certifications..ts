994b97f2482c99a98b09648c215e987aa5f356c9
LucasLuis-Dev <lucasluisouza@gmail.com>
2024-03-06 21:09:24 -0300

    fix: Certificate rendering fix

--- src/app/data/certifications..ts
+++ src/app/data/certifications..ts
@@ -1,94 +1,87 @@
 export const certifications = [
   {
     id: '1',
-    name: 'Certificado Python 3 - Mundo 1 | Curso em Vídeo',
-    image: 'https://cdn.discordapp.com/attachments/1139378969150509117/1139381182539235369/Python_Impressionador_-_Lucas_Luis_De_Souza_page-0001.jpg',
-    link: 'https://drive.google.com/drive/u/0/folders/1yVHdkSDjXNgEWJUJR0zNvxUThm15R34k',
-    stack: 'Python',
-  },
-  {
-    id: '2',
     name: 'Certificado Google Cloud Platform',
-    image: 'https://cdn.discordapp.com/attachments/1139378969150509117/1139381362969825352/BR-GCCF2022_-_UFPE_-_Universidade_Federal_de_Pernambuco-1_page-0001.jpg',
+    image: '../assets/images/certificados/BR-GCCF2022_-_UFPE_-_Universidade_Federal_de_Pernambuco-1_page-0001.jpg',
     link: 'https://drive.google.com/file/d/1gscJHjFXXd4KqabfWXPBXGRrpeybJhwT/view?usp=sharing',
     stack: 'Cloud'
   },
   {
-    id: '3',
+    id: '2',
     name: 'Certificado Potência Tech Angular Developer',
-    image: 'https://cdn.discordapp.com/attachments/1139378969150509117/1139382622846472222/Potencia_Tech_Angular_Developer_page-0001.jpg',
+    image: '../assets/images/certificados/Potencia_Tech_Angular_Developer_page-0001.jpg',
     link: 'https://drive.google.com/file/d/1d_0cASyFxArZnwf3A8BjH_9PVbhXCEOA/view?usp=sharing',
     stack: 'Angular'
   },
   {
-    id: '4',
+    id: '3',
     name: 'Certificado Linux do Zero',
-    image: 'https://cdn.discordapp.com/attachments/1139378969150509117/1139383355964674199/Certificado_Linux_do_Zero-1_page-0001.jpg',
+    image: '../assets/images/certificados/Certificado_Linux_do_Zero-1_page-0001.jpg',
     link: 'https://drive.google.com/file/d/1Iz3JWrT73lW3l8Iz9YGs67oNwo1JOOwR/view?usp=sharing',
     stack: 'Linux'
   },
   {
-    id: '5',
+    id: '4',
     name: 'Certificado Orange Tech +',
-    image: 'https://cdn.discordapp.com/attachments/1139378969150509117/1139578258095100034/Orange_Tech__page-0001.jpg',
+    image: '../assets/images/certificados/Orange_Tech__page-0001.jpg',
     link: 'https://drive.google.com/file/d/1nyTOy0KRIoGHIe_Zv7hONtsoUpOxA8VJ/view?usp=sharing',
     stack: 'Desenvolvimento Web'
   },
   {
-    id: '6',
+    id: '5',
     name: 'Certificado Front end - Alura ONE',
-    image: 'https://cdn.discordapp.com/attachments/1139378969150509117/1139607600745418833/Lucas_Luis_De_Souza_-_Formacao_-_Alura_page-00012.jpg',
+    image: '../assets/images/certificados/Lucas_Luis_De_Souza_-_Formacao_-_Alura_page-00012.jpg',
     link: 'https://drive.google.com/file/d/1PTvWizM3HJyZ9b8Ze9blst79txsC9rir/view?usp=sharing',
     stack: 'Desenvolvimento Web'
   },
   {
-    id: '7',
-    name: 'Certificado Começando no React',
-    image: 'https://cdn.discordapp.com/attachments/1139378969150509117/1139603977823072276/Comecando_na_biblioteca_React_page-0001.jpg',
-    link: 'https://drive.google.com/file/d/1JuJjtLvnJ7XsM1BK1bIjokxn_oh34VzG/view?usp=sharing',
-    stack: 'React'
-  },
-
-  {
-    id: '8',
+    id: '6',
     name: 'Certificado Formação JavaScript Developer',
-    image: 'https://cdn.discordapp.com/attachments/1139378969150509117/1139580566304464956/Formacao_JS_Developer_page-0001.jpg',
+    image: '../assets/images/certificados/Formacao_JS_Developer_page-0001.jpg',
     link: 'https://drive.google.com/file/d/1mZ4HQ9KzTxjbHEyIlimkflHQpw_t0p6i/view?usp=sharing',
     stack: 'JavaScript'
   },
   {
-    id: '9',
+    id: '7',
     name: 'Certificado Orange Tech + | Backend',
-    image: 'https://cdn.discordapp.com/attachments/1139378969150509117/1139579473008795678/Certificado_Orange_Tech__BackEnd-1_page-0001.jpg',
+    image: '../assets/images/certificados/Certificado_Orange_Tech__BackEnd-1_page-0001.jpg',
     link: 'https://drive.google.com/file/d/16R2c7BhxHdqLswoWAWMWYpIsmAtmgkqU/view?usp=sharing',
     stack: 'Java'
   },
   {
-    id: '10',
-    name: 'Certificado Conhecendo a Linguagem de Programação Java',
-    image: 'https://cdn.discordapp.com/attachments/1139378969150509117/1139598840165564498/Conhecendo_a_linguagem_Java-1_page-0001.jpg',
-    link: 'https://drive.google.com/file/d/1jqbEM0v6L85CvZn5RTHSHlP8Hs3Z618N/view?usp=sharing',
-    stack: 'Java'
-  },
-  {
-    id: '11',
-    name: 'Certificado Programação Orientada a Objetos com Java',
-    image: 'https://cdn.discordapp.com/attachments/1139378969150509117/1139598840459177994/POO_com_Java-1_page-0001.jpg',
-    link: 'https://drive.google.com/file/d/1igl_K1dju_shOm0pKxrHwca9a5fKqDDE/view?usp=sharing',
-    stack: 'Java'
-  },
-  {
-    id: '12',
+    id: '8',
     name: 'Certificado Estrutura de Dados e Algoritmos em Python',
-    image: 'https://cdn.discordapp.com/attachments/965066624556232737/1155485177733185586/UC-ebbce6d7-9c6d-423e-97cd-d6630fdfe2c3.jpg',
+    image: '../assets/images/certificados/Estrutura de Dados e Algoritmos (1)_page-0001.jpg',
     link: 'https://drive.google.com/file/d/1bVOvU38Ncp3i-p9syACkx101Yz_uoc4M/view?usp=sharing',
     stack: 'Python'
   },
   {
-    id: '13',
+    id: '9',
     name: 'Certificado Banco de Dados SQL e NOSQL',
-    image: "https://cdn.discordapp.com/attachments/965066624556232737/1156574792800014386/Certificado_Banco_de_Dados_SQL_e_NoSQL.jpg?ex=651577b6&is=65142636&hm=a1f9b8b3029d1698262be3c94f2317bfeee3b3e93c4a9e9fc41dbae8f793d311&",
+    image: "../assets/images/certificados/Certificado_Banco_de_Dados_SQL_e_NoSQL.jpg",
     link: "https://drive.google.com/file/d/1Td5wAUxHr-aQW-rHbunvGoe6izrsVfEU/view?usp=sharing",
     stack: 'Banco de Dados'
-  }
+  },
+  {
+    id: '10',
+    name: 'Certificado Python Impressionador',
+    image: "../assets/images/certificados/Python_Impressionador_-_Lucas_Luis_De_Souza_page-0001.jpg",
+    link: "https://portalhashtag.com/certificado-hashtag/1668373878545x381228890406415800",
+    stack: 'Python'
+  },
+  {
+    id: '11',
+    name: 'Certificado Linux Fundamentals',
+    image: "../assets/images/certificados/4Linux - Lucas Luis De Souza_page-0001.jpg",
+    link: "https://drive.google.com/file/d/1JEKMFbrNqtAxew4NG68fw5Pb3TzubQG4/view?usp=sharing",
+    stack: 'Linux'
+  },
+  {
+    id: '12',
+    name: 'Certificado Linux Essentials',
+    image: "../assets/images/certificados/4Linux - Lucas Luis De Souza Linux essentials)_page-0001.jpg",
+    link: "https://drive.google.com/file/d/1VSnE6f0lmGGRlOIqKwofi5OrbotkLWTR/view?usp=sharing",
+    stack: 'Linux'
+  },
+  
 ]
