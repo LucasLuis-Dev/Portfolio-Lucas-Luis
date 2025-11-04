ac729ec995d28e1274f5add0ab72e5aba4e491a7
Lucas luis <lucasluisouza@gmail.com>
2025-11-04 16:09:59 -0300

    feat: adding a new phase to my career and new certifications

--- src/app/data/certifications..ts
+++ src/app/data/certifications..ts
@@ -8,80 +8,93 @@ export const certifications = [
   },
   {
     id: '2',
+    name: "Certificado Ministrante do Rec'n'Play 2025",
+    image: '../assets/images/certificados/certificados_rnp_ministrante_png_B07WM2TE5ENR.jpg',
+    link: 'https://drive.google.com/file/d/1PFHOfJsjKbbFqUXamSrq8fce-tFASZUm/view?usp=sharing',
+    stack: ''
+  },
+   {
+    id: '3',
+    name: "Certificado 2° Lugar na OPEI 2022",
+    image: '../assets/images/certificados/Certificado OPEI 2022.jpg',
+    link: 'https://drive.google.com/file/d/13SOaOrMx8oxfnkdz4l9DKLB6yYy44hHT/view?usp=sharing',
+    stack: ''
+  },
+  {
+    id: '4',
     name: 'Certificado Potência Tech Angular Developer',
     image: '../assets/images/certificados/Potencia_Tech_Angular_Developer_page-0001.jpg',
     link: 'https://drive.google.com/file/d/1d_0cASyFxArZnwf3A8BjH_9PVbhXCEOA/view?usp=sharing',
     stack: 'Angular'
   },
   {
-    id: '3',
+    id: '5',
     name: 'Certificado Linux do Zero',
     image: '../assets/images/certificados/Certificado_Linux_do_Zero-1_page-0001.jpg',
     link: 'https://drive.google.com/file/d/1Iz3JWrT73lW3l8Iz9YGs67oNwo1JOOwR/view?usp=sharing',
     stack: 'Linux'
   },
   {
-    id: '4',
+    id: '6',
     name: 'Certificado Orange Tech +',
     image: '../assets/images/certificados/Orange_Tech__page-0001.jpg',
     link: 'https://drive.google.com/file/d/1nyTOy0KRIoGHIe_Zv7hONtsoUpOxA8VJ/view?usp=sharing',
     stack: 'Desenvolvimento Web'
   },
   {
-    id: '5',
+    id: '7',
     name: 'Certificado Front end - Alura ONE',
     image: '../assets/images/certificados/Lucas_Luis_De_Souza_-_Formacao_-_Alura_page-00012.jpg',
     link: 'https://drive.google.com/file/d/1PTvWizM3HJyZ9b8Ze9blst79txsC9rir/view?usp=sharing',
     stack: 'Desenvolvimento Web'
   },
   {
-    id: '6',
+    id: '8',
     name: 'Certificado Formação JavaScript Developer',
     image: '../assets/images/certificados/Formacao_JS_Developer_page-0001.jpg',
     link: 'https://drive.google.com/file/d/1mZ4HQ9KzTxjbHEyIlimkflHQpw_t0p6i/view?usp=sharing',
     stack: 'JavaScript'
   },
   {
-    id: '7',
+    id: '9',
     name: 'Certificado Orange Tech + | Backend',
     image: '../assets/images/certificados/Certificado_Orange_Tech__BackEnd-1_page-0001.jpg',
     link: 'https://drive.google.com/file/d/16R2c7BhxHdqLswoWAWMWYpIsmAtmgkqU/view?usp=sharing',
     stack: 'Java'
   },
   {
-    id: '8',
+    id: '10',
     name: 'Certificado Estrutura de Dados e Algoritmos em Python',
     image: '../assets/images/certificados/Estrutura de Dados e Algoritmos (1)_page-0001.jpg',
     link: 'https://drive.google.com/file/d/1bVOvU38Ncp3i-p9syACkx101Yz_uoc4M/view?usp=sharing',
     stack: 'Python'
   },
   {
-    id: '9',
+    id: '11',
     name: 'Certificado Banco de Dados SQL e NOSQL',
     image: "../assets/images/certificados/Certificado_Banco_de_Dados_SQL_e_NoSQL.jpg",
     link: "https://drive.google.com/file/d/1Td5wAUxHr-aQW-rHbunvGoe6izrsVfEU/view?usp=sharing",
     stack: 'Banco de Dados'
   },
   {
-    id: '10',
+    id: '12',
     name: 'Certificado Python Impressionador',
     image: "../assets/images/certificados/Python_Impressionador_-_Lucas_Luis_De_Souza_page-0001.jpg",
     link: "https://portalhashtag.com/certificado-hashtag/1668373878545x381228890406415800",
     stack: 'Python'
   },
   {
-    id: '11',
+    id: '13',
     name: 'Certificado Linux Fundamentals',
     image: "../assets/images/certificados/4Linux - Lucas Luis De Souza_page-0001.jpg",
     link: "https://drive.google.com/file/d/1JEKMFbrNqtAxew4NG68fw5Pb3TzubQG4/view?usp=sharing",
     stack: 'Linux'
   },
   {
-    id: '12',
+    id: '14',
     name: 'Certificado Linux Essentials',
     image: "../assets/images/certificados/4Linux - Lucas Luis De Souza Linux essentials)_page-0001.jpg",
     link: "https://drive.google.com/file/d/1VSnE6f0lmGGRlOIqKwofi5OrbotkLWTR/view?usp=sharing",
     stack: 'Linux'
   },
-  
 ]
