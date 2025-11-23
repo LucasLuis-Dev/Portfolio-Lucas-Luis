d4f44899d8127e9a824d11b6227fdc90ea05fdbb
LucasLuis-Dev <lucasluisouza@gmail.com>
2025-11-23 13:49:41 -0300

    feat: addition of the new investView project

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -19,6 +19,15 @@ export const dataFake = [
   },
   {
     "id": "3",
+    "photos": ["../../../../../assets/images/invest-mobile.jpg","../../../../../assets/images/invest-notebook.jpg", "../../../../../assets/images/invest-netbook.jpg"],
+    "title": "InvestView",
+    "description": "InvestView é uma plataforma web desenvolvida em Vue.js que simplifica o acesso e a visualização de dados do mercado de ações brasileiro. O projeto oferece uma interface intuitiva e moderna para investidores que buscam acompanhar cotações, gráficos históricos e informações fundamentalistas de ativos da bolsa de valores de forma descomplicada.",
+    "techs": ['vuejs', 'git','typescript', 'sass'],
+    "website": "https://invest-map.vercel.app/m",
+    "github": "https://github.com/LucasLuis-Dev/InvestMap"
+  },
+  {
+    "id": "4",
     "photos": ["../../../../../assets/images/gpt4enem-mobile.jpg","../../../../../assets/images/gpt4enem-notebook.jpg", "../../../../../assets/images/gpt4enem-netbook.jpg"],
     "title": "GPT4ENEM",
     "description": "O GPT4ENEM foi desenvolvido para oferecer aos estudantes do ensino médio uma ferramenta prática e eficaz para acessar conteúdos relacionados ao Exame Nacional do Ensino Médio (ENEM). Ele possibilita resumir vídeos, transcrever áudio e a verificação de cobertura de conteudo, tudo em uma plataforma única e gratuita.",
@@ -27,7 +36,7 @@ export const dataFake = [
     "github": "https://github.com/EngSoftware23/GPT4ENEM"
   },
   {
-    "id": "4",
+    "id": "5",
     "photos": ["../../../../../assets/images/topsis-mobile.jpg","../../../../../assets/images/topsis-notebook.jpg", "../../../../../assets/images/topsis-netbook.jpg"],
     "title": "TOPSIS",
     "description": "Essa é uma aplicação que oferece suporte inteligente à tomada de decisão por meio do algoritmo TOPSIS, disponível de forma intuitiva e acessível. Simplificando suas escolhas com precisão e confiabilidade, independentemente do contexto ou complexidade da análise.",
@@ -48,7 +57,7 @@ export const dataFake = [
   },
   */
   {
-    "id": "5",
+    "id": "6",
     "photos": ["../../../../../assets/images/hotcoffee-mobile.jpg","../../../../../assets/images/hotcoffee-notebook.jpg", "../../../../../assets/images/hotcoffee-netbook.jpg"],
     "title": "HotCoffee",
     "description": "A aplicação Angular em desenvolvimento na Vercel é uma plataforma de busca de notícias de tecnologia, alimentada pela API do News API. Os usuários podem realizar pesquisas específicas e visualizar resultados de notícias de maneira intuitiva. ",
@@ -57,7 +66,7 @@ export const dataFake = [
     "github": "https://github.com/LucasLuis-Dev/Blog"
   },
   {
-    "id": "6",
+    "id": "7",
     "photos": ["../../../../../assets/images/sysgreen-mobile.jpg","../../../../../assets/images/sysgreen-notebook.jpg", "../../../../../assets/images/sysgreen-netbook.jpg"],
     "title": "SysGreen",
     "description": "O SysGreen foi desenvolvido para oferecer aos cidadãos uma ferramenta prática e eficaz para gerenciar a reciclagem de resíduos eletrônicos. Ele possibilita o agendamento de coletas, o acompanhamento do processo de reciclagem e a troca de resíduos por pontos, tudo em uma plataforma única e gratuita.",
@@ -66,7 +75,7 @@ export const dataFake = [
     "github": "https://github.com/LucasLuis-Dev/SysGreen"
   },
   {
-    "id" : "7",
+    "id" : "8",
     "photos": ["../../../../../assets/images/dev-community-mobile.jpg","../../../../../assets/images/dev-community-notebook.jpg", "../../../../../assets/images/dev-community-netbook.jpg"],
     "title": "Dev Community",
     "description": "Desenvolvi o Dev Community, uma aplicação web que proporciona interação e colaboração entre desenvolvedores. Utilizei o framework Flask para criar a aplicação do zero, implementando recursos como autenticação de usuários e integração com banco de dados. ",
@@ -75,7 +84,7 @@ export const dataFake = [
     "github": "https://github.com/LucasLuis-Dev/Dev-Community"
   },
   {
-    "id": "8",
+    "id": "9",
     "photos": ["../../../../../assets/images/algoritmo-kruskal-mobile.jpg","../../../../../assets/images/algoritmo-kruskal-notebook.jpg", "../../../../../assets/images/algoritmo-kruskal-netbook.jpg"],
     "title": "Algoritmo de Kruskal",
     "description": "Este projeto trata de um problema ilustrativo onde há preocupações de segurança da comunicação por e-mail em uma instituição, identificando conexões importantes e confiáveis entre os membros.",
@@ -84,7 +93,7 @@ export const dataFake = [
     "github": "https://github.com/LucasLuis-Dev/Algoritmo-de-Kruskal"
   },
   {
-    "id": "9",
+    "id": "10",
     "photos": ["../../../../../assets/images/space-attack-mobile.jpg","../../../../../assets/images/space-attack-notebook.jpg","../../../../../assets/images/space-attack-netbook.jpg"],
     "title": "Space Attack",
     "description": "Space Attack é um jogo interativo baseado em Star Wars, onde o jogador controla uma nave e enfrenta asteróides e naves inimigas.",
@@ -93,7 +102,7 @@ export const dataFake = [
     "github": "https://github.com/LucasLuis-Dev/Projeto-P1-Space-Attack"
   },
   {
-    "id": "10",
+    "id": "11",
     "photos": ["../../../../../assets/images/doctor-care-mobile.jpg","../../../../../assets/images/doctor-care-notebook.jpg", "../../../../../assets/images/doctor-care-netbook.jpg"],
     "title": "Doctor Care",
     "description": "Projeto de Website pensado para Clinicas de saúde feito em conjunto no evento NLW Return proporcionado pela Rocketseat.",
